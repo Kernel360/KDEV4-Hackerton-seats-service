5df9e1b01eae3507a3257d67308982a07714b7d8
seoyeon-jung <jungluchia1204@gmail.com>
2025-03-11 20:49:07 +0900

    feat: 현재 예약 리스트 API 연동

---
+++ frontend/src/components/main/util/SeatUtil.ts
@@ -0,0 +1,22 @@
+export const DEFAULT_SEATS = [
+  {
+    id: '1',
+    name: '좌석 A',
+    isUsed: false
+  },
+  {
+    id: '2',
+    name: '좌석 B',
+    isUsed: false
+  },
+  {
+    id: '3',
+    name: '좌석 C',
+    isUsed: false
+  },
+  {
+    id: '4',
+    name: '좌석 D',
+    isUsed: false
+  }
+]
