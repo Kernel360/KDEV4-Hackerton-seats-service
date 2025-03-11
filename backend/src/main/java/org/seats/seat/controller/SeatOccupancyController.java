ff68d4b700046ef262ca2cdc1ae4492cdc99f96b
seoyeon-jung <jungluchia1204@gmail.com>
2025-03-11 10:23:21 +0900

    feat: 예약 취소 API 구현

--- backend/src/main/java/org/seats/seat/controller/SeatOccupancyController.java
+++ backend/src/main/java/org/seats/seat/controller/SeatOccupancyController.java
@@ -1,12 +1,17 @@
 package org.seats.seat.controller;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.seats.seat.domain.OccupancyListResponse;
 import org.seats.seat.domain.OccupancyRequest;
 import org.seats.seat.domain.OccupancyResponse;
 import org.seats.seat.repository.SeatOccupancyRepository;
 import org.seats.seat.service.SeatOccupancyService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,10 +24,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class SeatOccupancyController {
 
     private final SeatOccupancyService seatOccupancyService;
-    private final SeatOccupancyRepository seatOccupancyRepository;
 
     // 전체 예악 현황 보기
-
+    
     // 현재 예약 상황 리스트
 
     // 예약 하기
@@ -32,17 +36,10 @@ public class SeatOccupancyController {
         return ResponseEntity.ok(response);
     }
 
-    // 예약 수정
-    @PutMapping("")
-    public ResponseEntity<OccupancyResponse> updateOccupancy(@RequestBody OccupancyRequest request) {
-        OccupancyResponse response = seatOccupancyService.updateOccupancy(request);
-        return ResponseEntity.ok(response);
-    }
-
     // 예악 취소
-    @DeleteMapping("")
-    public void delete() {
-        seatOccupancyRepository.deleteAll();
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        seatOccupancyService.delete(id);
     }
 
 }
--- backend/src/main/java/org/seats/seat/service/SeatOccupancyService.java
+++ backend/src/main/java/org/seats/seat/service/SeatOccupancyService.java
@@ -3,9 +3,12 @@ package org.seats.seat.service;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.seats.seat.converter.SeatOccupancyConverter;
+import org.seats.seat.domain.OccupancyListResponse;
 import org.seats.seat.domain.OccupancyRequest;
 import org.seats.seat.domain.OccupancyResponse;
 import org.seats.seat.entity.Seat;
@@ -23,6 +26,11 @@ public class SeatOccupancyService {
     private final SeatRepository seatRepository;
     private final SeatOccupancyConverter seatOccupancyConverter;
 
+    // 예약 리스트 전체 조회
+    public List<OccupancyListResponse> getAllList() {
+        return null;
+    }
+
     // 예약 하기
     public OccupancyResponse createOccupancy(OccupancyRequest request) {
         // 요청된 시간대 시작 시간 파싱
@@ -37,28 +45,32 @@ public class SeatOccupancyService {
         LocalDateTime startOfDay = requestedDate.atStartOfDay();  // 하루의 시작
         LocalDateTime endOfDay = requestedDate.plusDays(1).atStartOfDay();  // 하루의 끝 (다음날 00:00)
 
-        log.info("Checking availability for seat: {} from {} to {}", seat.getId(), startOfDay, endOfDay);
-
         // 같은 날짜와 좌석에 대해 예약이 2번을 초과했는지 확인
-        long bookingCount = seatOccupancyRepository.countBySeatAndStartTimeBetween(seat, startOfDay, endOfDay);
-
-        // 이미 하루에 2번 예약이 되어 있는지 체크
-        if (bookingCount >= 2) {
-            log.error("Attempt to book seat {} on {} when it is already booked twice.", seat.getId(), requestedDate);
-            throw new IllegalStateException("하루에 2번 예약 가능합니다.");
-        }
+        checkBookingLimit(seat, startOfDay, endOfDay, requestedDate);
 
         SeatOccupancy occupancy = seatOccupancyConverter.toEntity(request);
         SeatOccupancy newOccupancy = seatOccupancyRepository.save(occupancy);
 
         // 예약 성공 시, 응답 객체로 변환하여 반환
         return seatOccupancyConverter.toResponse(newOccupancy);
-
     }
 
+    // 예약이 하루에 2번 초과하는지 확인하는 메서드
+    private void checkBookingLimit(Seat seat, LocalDateTime startOfDay, LocalDateTime endOfDay, LocalDate requestedDate) {
+        long bookingCount = seatOccupancyRepository.countBySeatAndStartTimeBetween(seat, startOfDay, endOfDay);
 
-    // 예약 정보 수정하기
-    public OccupancyResponse updateOccupancy(OccupancyRequest request) {
-        return null;
+        // 이미 하루에 2번 예약이 되어 있는지 체크
+        if (bookingCount >= 2) {
+            log.error("Attempt to book or update seat {} on {} when it is already booked twice.", seat.getId(), requestedDate);
+            throw new IllegalStateException("하루에 2번 예약 가능합니다.");
+        }
+    }
+
+    // 삭제하기
+    public void delete(Long id) {
+        SeatOccupancy seatOccupancy = seatOccupancyRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("일치하는 예약이 없습니다"));
+
+        seatOccupancyRepository.deleteById(id);
     }
 }
