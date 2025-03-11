3183aba4919110a5417235eaa83cdae00002f3af
kimchijinju <hanbimkim@gmail.com>
2025-03-11 16:27:19 +0900

    fix: 컴파일 해결

--- backend/src/main/java/org/seats/seat/converter/SeatOccupancyConverter.java
+++ backend/src/main/java/org/seats/seat/converter/SeatOccupancyConverter.java
@@ -2,7 +2,7 @@ package org.seats.seat.converter;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import lombok.RequiredArgsConstructor;
+
 import org.seats.seat.domain.MyOccupancyListResponse;
 import org.seats.seat.domain.OccupancyRequest;
 import org.seats.seat.domain.OccupancyResponse;
@@ -10,46 +10,48 @@ import org.seats.seat.entity.Seat;
 import org.seats.seat.entity.SeatOccupancy;
 import org.seats.seat.repository.SeatRepository;
 import org.seats.user.entity.User;
-import org.seats.user.repository.UserRepository;
+import org.seats.user.entity.UserRepository;
 import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Component
 public class SeatOccupancyConverter {
-    private final UserRepository userRepository;
-    private final SeatRepository seatRepository;
+	private final UserRepository userRepository;
+	private final SeatRepository seatRepository;
 
-    public SeatOccupancy toEntity(OccupancyRequest request) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+	public SeatOccupancy toEntity(OccupancyRequest request) {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
-        User user = userRepository.findById(request.getUserId())
-            .orElseThrow(() -> new RuntimeException("User Not Found"));
+		User user = userRepository.findById(request.getUserId())
+			.orElseThrow(() -> new RuntimeException("User Not Found"));
 
-        Seat seat = seatRepository.findById(request.getSeatId())
-            .orElseThrow(() -> new RuntimeException("Seat Not Found"));
+		Seat seat = seatRepository.findById(request.getSeatId())
+			.orElseThrow(() -> new RuntimeException("Seat Not Found"));
 
-        return SeatOccupancy.builder()
-            .user(user)
-            .seat(seat)
-            .startTime(LocalDateTime.parse(request.getStartTime(), formatter))
-            .build();
-    }
+		return SeatOccupancy.builder()
+			.user(user)
+			.seat(seat)
+			.startTime(LocalDateTime.parse(request.getStartTime(), formatter))
+			.build();
+	}
 
-    public OccupancyResponse toResponse(SeatOccupancy seatOccupancy) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+	public OccupancyResponse toResponse(SeatOccupancy seatOccupancy) {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
-        return OccupancyResponse.builder()
-            .userId(seatOccupancy.getUser().getId())
-            .seatId(seatOccupancy.getSeat().getId())
-            .startTime(seatOccupancy.getStartTime().format(formatter))
-            .build();
-    }
+		return OccupancyResponse.builder()
+			.userId(seatOccupancy.getUser().getId())
+			.seatId(seatOccupancy.getSeat().getId())
+			.startTime(seatOccupancy.getStartTime().format(formatter))
+			.build();
+	}
 
-    public MyOccupancyListResponse toMyOccupancyListResponse(SeatOccupancy seatOccupancy) {
-        return MyOccupancyListResponse.builder()
-            .seatId(seatOccupancy.getSeat().getId())
-            .seatName(seatOccupancy.getSeat().getName()) // 좌석 이름
-            .startTime(seatOccupancy.getStartTime().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"))) // 시작 시간 포맷
-            .build();
-    }
+	public MyOccupancyListResponse toMyOccupancyListResponse(SeatOccupancy seatOccupancy) {
+		return MyOccupancyListResponse.builder()
+			.seatId(seatOccupancy.getSeat().getId())
+			.seatName(seatOccupancy.getSeat().getName()) // 좌석 이름
+			.startTime(seatOccupancy.getStartTime().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"))) // 시작 시간 포맷
+			.build();
+	}
 }
